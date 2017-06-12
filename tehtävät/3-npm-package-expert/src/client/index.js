81705d669e77d0f116389aa17f9ad5dd47594ed0
Arno Saine <arno@mowhi.com>
2017-06-12 18:54:12 +0300

    Korttien haku, listaus ja pelin aloitus.

--- tehtävät/3-npm-package-expert/src/client/index.js
+++ tehtävät/3-npm-package-expert/src/client/index.js
@@ -2,30 +2,58 @@ const button = (onClick, children) =>
   `<button onClick="${onClick}">${children}</button>`;
 
 const h1 = children => `<h1>${children}</h1>`;
+const cardTitle = ({ name, version }) => `<li>${name}@${version}</li>`;
+const card = ({ name, version, ...statistics }) => `
+  <h3>${name}@${version}</h3>
+  <ul>
+    ${Object.entries(statistics)
+      .map(([prop, value]) => `<li>${prop}: ${value}</li>`)
+      .join('')}
+  </ul>
+`;
+
+// https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+function shuffle(a) {
+  for (let i = a.length; i; i--) {
+    let j = Math.floor(Math.random() * i);
+    [a[i - 1], a[j]] = [a[j], a[i - 1]];
+  }
+}
 
 class App {
   constructor(root) {
     this.root = root;
-    this.counter = 0;
+    this.playerACards = [];
+    this.playerBCards = [];
     this.update();
+    this.getCards();
   }
   update() {
     this.root.innerHTML = this.render();
   }
-  increase() {
-    this.counter++;
-    this.update();
+  getCards() {
+    fetch('/api/top-packages.json')
+      .then(response => response.json())
+      .then(cards => {
+        this.cards = cards;
+        this.update();
+      });
   }
-  decrease() {
-    this.counter--;
+  start() {
+    shuffle(this.cards);
+    this.playerACards = this.cards.slice(0, this.cards.length / 2);
+    this.playerBCards = this.cards.slice(this.cards.length / 2);
     this.update();
   }
   render() {
     return `
-      ${h1('Counter')}
-      <div>${this.counter}</div>
-      ${button('app.decrease()', '-')}
-      ${button('app.increase()', '+')}
+      ${h1('npm Package Expert')}
+      ${button('app.start()', 'Start')}
+      ${this.cards ? `<ul>${this.cards
+            .map(cardTitle)
+            .join('')}</ul>` : 'Loading...'}
+      ${/* Molemmilla pelaajilla on kortteja? */
+      this.playerACards.length && this.playerBCards.length ? card(this.playerACards[0]) : ''}
     `;
   }
 }
